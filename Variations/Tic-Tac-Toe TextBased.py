a71609c1fd10a5d80781aebbed0d730f570656d7
Hermes Bonilla <hboni008@fiu.edu>
2020-08-02 19:14:48 -0400

    Add all winning logic, gameboard logic, error handling

--- Variations/Tic-Tac-Toe TextBased.py
+++ Variations/Tic-Tac-Toe TextBased.py
@@ -1,10 +1,7 @@
+#Text-Based Tic Tac Toe by Hermes B.
 
-# coding: utf-8
-
-# In[ ]:
-
-
-#game of tic tac toe by Hermes B.
+#importing itertools to cycle between players
+import itertools
 
 #initializing initial game map
 game = [[0, 0, 0],
@@ -14,30 +11,50 @@ game = [[0, 0, 0],
 
 
 def game_board(game_map, player=0, row=0, column=0, just_display=False):
+    """
+        Scalable game board size (up to 10)
+    """
     try:
+        if game_map[row][column] != 0:
+            print("This position is occupied! Choose another space.")
+            return game_map, False
+        
         numberedRow = enumerate(game_map)
-        print("   A  B  C")
+        print("   "+"  ".join([str(i) for i in range(len(game_map))]))
         if not just_display:
             game_map[row][column] = player
         for count, row in numberedRow:
             print(count, row)
         print("              ")
-        return game_map
+        return game_map, True
     
     #Error Handling in function
     except IndexError as e:
         print("Something went wrong... did you input row/column as 0, 1, or 2?", "(", e, ")")
+        return game_map, False
         
     except Exception as e:
         print("Something went very wrong!", "(", e, ")")
+        return game_map, False
     
     
 def win(current_game):
+    """
+        Winning logic 
+    """
+    #Function condenses repeated logic
+    def all_same(L):
+        if L.count(L[0]) == len(L) and L[0] != 0:
+            return True
+        else:
+            return False
+        
     #Horizontal logic
     for row in game:
         print(row)
-        if row.count(row[0]) == len(row) and row[0] != 0:
+        if all_same(row):
             print(f"Player {row[0]} is the winner horizontally (⁠—)!")
+            return True
     
     #Diagonal logic
     diags = []
@@ -45,45 +62,60 @@ def win(current_game):
     rows = range(len(game))
     for col, row in zip(cols, rows):
         diags.append(game[row][col])
-    if diags.count(diags[0]) == len(diags) and diags[0] != 0:
+    if all_same(diags):
             print(f"Player {diags[0]} is the winner diagonally (/)!")
+            return True
             
     diags = []    
     for index in range(len(game)):
         diags.append(game[index][index])
-    if diags.count(diags[0]) == len(diags) and diags[0] != 0:
+    if all_same(diags):
             print(f"Player {diags[0]} is the winner diagonally (\\)!")
+            return True
         
     #Vertical logic
     for col in range(len(game)):
         check = []
-        
         for row in game:
             check.append(row[col])
         
-        if check.count(check[0]) == len(check) and check[0] != 0:
+        if all_same(check):
             print(f"Player {check[0]} is the winner vertically (|)!")
-    
+            return True
+    return False
     
 play = True
 player = [1, 2]
 while play:
-    game = [[0, 0, 0],
-            [0, 0, 0],
-            [0, 0, 0]]
+    
+    game_size = int(input("What size game of tic tac toe? (2-10) \nExample: if 3, then the game will be a 3 by 3 square! \nYour input: "))
+    game = [[0 for i in range(game_size)] for i in range(game_size)]
+    
     game_won = False
-    game = game_board(game, just_display=True)
+    game, _ = game_board(game, just_display=True)
+    player_choice = itertools.cycle([1, 2])
     while not game_won:
-        current_player = 1
-        column_choice = input("On which column do you want to play? (0, 1, 2): ")
-        row_choice = input("On which row do you want to play? (0, 1, 2): ")
-        game = game_board(game, current_player, row_choice, column_choice)
+        current_player = next(player_choice)
+        print(f"\nCurrent Player: {current_player}")
+        played = False
+        
+        while not played:
+            column_choice = int(input("On which column do you want to play? (0, 1, 2): "))
+            row_choice = int(input("On which row do you want to play? (0, 1, 2): "))
+            game, played = game_board(game, current_player, row_choice, column_choice)
+            
+        if win(game):
+            game_won = True
+            again = input("The game is over, ready for another round? (yes/no) \nYour input: ")
+            if again.lower() == "yes":
+                print("restarting...")
+            elif again.lower() == "no":
+                print("Hope you come again!")
+                play = False
+            else:
+                print("Not a valid answer, so... Bye!")
+                play = False
+                
     
-    
-
-
-# In[ ]:
-
-
 
 
